b976d4015aa0bdd71277cc42b120244ac42ab303
Jake Alsemgeest <alsemgeester@gmail.com>
2015-05-28 02:25:46 -0400

    Initial commit of the site.

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="css/custom.css" rel="stylesheet">
+</head>
+<body>
+	<section class="top">
+		<div class="centered-top">
+			<div class="top-image"></div>
+			<div class="centered-top-table">
+				<div class="top-box">
+					<div class="top-content-background"></div>
+					<div class="top-content">
+						<h1>Richard Purvis</h1>
+						<div class="social-content">
+							<ul>
+								<li>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-github"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-pinterest"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-legal"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="rest">
+		<section class="about-me">
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate mauris ut maximus posuere. Sed posuere purus sed ex malesuada molestie. Vivamus ac lorem id nisi viverra sodales. Sed massa neque, tempor vel nibh sed, venenatis blandit felis. Nam ornare ligula id lobortis cursus. Maecenas pharetra volutpat mi, vel maximus turpis posuere id. Cras commodo porta massa. Ut lobortis lectus sagittis faucibus condimentum. Aliquam eget lacinia risus. Praesent interdum velit vel ullamcorper porttitor posuere.
+			</p>
+		</section>
+		<section class="content">
+			<ul><li><p>Content</p></li><li><p>Content</p></li><li><p>Content</p></li><li><p>Content</p></li></ul>
+		</section>
+	</section>
+	<footer>
+		
+	</footer>
+</body>	
+</html>
