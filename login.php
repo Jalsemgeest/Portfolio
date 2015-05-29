bfdd0f21c952bd4f3bf0b3dc06a94bd9a232f030
Jake Alsemgeest <alsemgeester@gmail.com>
2015-05-29 12:08:59 -0400

    Added footer to the login and index page.

--- login.php
+++ login.php
@@ -10,43 +10,44 @@
 	<link href="css/custom.css" rel="stylesheet">
 </head>
 <body>
-	<section class="top">
-		<div class="centered-top">
-			<div class="top-image"></div>
-			<div class="centered-top-table">
-				<div class="top-box">
-					<div class="top-content-background"></div>
-					<div class="top-content">
-						<h1>Login</h1>
+	<section class="main-content">
+		<section class="top">
+			<div class="centered-top">
+				<div class="top-image"></div>
+				<div class="centered-top-table">
+					<div class="top-box">
+						<div class="top-content-background"></div>
+						<div class="top-content">
+							<h1>Login</h1>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
-	<section class="rest">
-		<section class="about-me">
-			<form class="login-form" method="post" action="login-exec.php">
-				<div class="login-email">
-					<label for="email">Email:</label>
-					<input name="email" type="email" id="email" required/>
-				</div>
-				<div class="login-password">
-					<label for="password">Password:</label>
-					<input name="password" type="password" id="password" required/>
-				</div>
-				<div class="login-button-area">
-					<button type="submit">Login</button>
-					<input name="remember_me" id="remember_me" type="checkbox"/>
-					<label for="remember_me">Remember Me</label>
-				</div>
-			</form>
 		</section>
-		<section class="content">
-			<ul><li><p>Content</p></li><li><p>Content</p></li><li><p>Content</p></li><li><p>Content</p></li></ul>
+		<section class="rest">
+			<section class="about-me">
+				<form class="login-form" method="post" action="login-exec.php">
+					<div class="login-email">
+						<label for="email">Email:</label>
+						<input name="email" type="email" id="email" required/>
+					</div>
+					<div class="login-password">
+						<label for="password">Password:</label>
+						<input name="password" type="password" id="password" required/>
+					</div>
+					<div class="login-button-area">
+						<button type="submit">Login</button>
+						<input name="remember_me" id="remember_me" type="checkbox"/>
+						<label for="remember_me">Remember Me</label>
+					</div>
+				</form>
+			</section>
 		</section>
 	</section>
 	<footer>
-		
+		<div class="footer-content">
+			<p>Profolio.com</p>
+		</div>
 	</footer>
 </body>	
 </html>
